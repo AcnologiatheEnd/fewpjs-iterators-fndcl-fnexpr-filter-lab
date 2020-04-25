6fcded58b5a86e530724a359ca816511bf8c52c3
Winston  Chung <18.chung.winston@ctkprep.org>
2020-04-25 12:07:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function findMatching (list, name) {
-  return list.filter(function (driverName) {
-    return driverName.toLowerCase() === name.toLowerCase();
+  return list.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
   });
+}
+
+function fuzzyMatch(list, partialName) {
+  let lengthOfName = partialName.length
+  return list.filter(function(driver) {
+    return driver.slice(0, lengthOfName) === partialName
+  })
 }
