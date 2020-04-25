088f1cee22653dfb14d152fabe694d8d859ea5ea
Winston  Chung <18.chung.winston@ctkprep.org>
2020-04-25 12:23:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching (list, name) {
+  return list.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list, partialName) {
+  let lengthOfName = partialName.length
+  return list.filter(function(driver) {
+    return driver.slice(0, lengthOfName) === partialName
+  })
+}
+
+function matchName(list, name) {
+  return list.filter(function(driver) {
+    return driver.name.toLowerCase() === name.toLowerCase()
+  })
+}
